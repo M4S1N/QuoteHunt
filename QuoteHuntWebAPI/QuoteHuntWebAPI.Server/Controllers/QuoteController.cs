2a1eb2bf43666f6e2631efc74e0e0d8634d3ef59
Miguel Asin Barthelemy <79877427+M4S1N@users.noreply.github.com>
2025-08-12 11:06:49 -0600

    API-001/Add API structure
    - Create initial structure
    - Add Controller, DTO and configuration

---
+++ QuoteHuntWebAPI/QuoteHuntWebAPI.Server/Controllers/QuoteController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using QuoteHuntWebAPI.DTO;
+
+namespace QuoteHuntWebAPI.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuoteController(
+        ILogger<QuoteController> logger,
+        IOptions<ScraperSetting> scraperSetting
+    ) : ControllerBase
+    {
+        public readonly ILogger<QuoteController> _logger = logger;
+        public readonly ScraperSetting _scraperSetting = scraperSetting.Value;
+    }
+}
---
+++ QuoteHuntWebAPI/QuoteHuntWebAPI.Server/Program.cs
@@ -0,0 +1,28 @@
+using QuoteHuntWebAPI.DTO;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<ScraperSetting>(
+    builder.Configuration.GetSection("ScraperSettings")
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
