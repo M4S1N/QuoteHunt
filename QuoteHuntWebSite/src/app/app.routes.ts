36becb55f39131981bb682d554f3a6d50e2077fe
Miguel Asin Barthelemy <79877427+M4S1N@users.noreply.github.com>
2025-08-13 11:13:03 -0600

    FRONT-001/Change styles, define routes, and create component

--- QuoteHuntWebSite/src/app/app.routes.ts
+++ QuoteHuntWebSite/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { QuoteComponent } from './modules/quote/components/quote.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('./modules/quote/quote.routes').then(m => m.routes)
+    }
+];
